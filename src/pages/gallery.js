bc11a54d9b4ad5df4c2d2bfe5f054eae7ee2b1f0
akinaga <yoakemae256@gmail.com>
2021-09-23 23:47:51 +0900

    gallery改修

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -7,6 +7,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import SvgIcon from '@material-ui/core/SvgIcon';
+
 import _2013 from '../config/gallery/2013.json'
 import _2014 from '../config/gallery/2014.json'
 import _2015 from '../config/gallery/2015.json'
@@ -18,16 +19,25 @@ import _2020 from '../config/gallery/2020.json'
 
 const CardItems = ({ card, year, allPhotoShow }) => {
 
-    const Cards = []
+    // 「1-9」 -> 「01-09」 に変換する
     const returnZeroIdx = (idx) => (idx < 10 ? '0' + String(idx) : String(idx))
 
-    let length = 3
-    if (String(allPhotoShow) === card.id) length = card.itemNum
+    // 一致する月の表示状況stateをphotoShowに設定
+    let photoShow = false
+    allPhotoShow.forEach(monthShow => { if (monthShow[card.id]) photoShow = monthShow[card.id] })
+    
+    // 表示するカード数を設定
+    let cardNum = 3
+    if (photoShow) cardNum = card.itemNum
 
-    for (let idx = 1; idx <= length; idx++) {
+    // コンポーネント作成
+    const Cards = []
+    for (let idx = 1; idx <= cardNum; idx++) {
 
-        let fileName = String(year) + '-' + returnZeroIdx(card.id) + '_' + returnZeroIdx(idx) + '.jpg'
+        // ファイル名設定
+        const fileName = `${String(year)}-${returnZeroIdx(card.id)}_${returnZeroIdx(idx)}.jpg`
 
+        // コンポーネントを配列に追加
         Cards.push(
             <Card
                 key={`${ card.month }_${ idx }`}
@@ -37,39 +47,44 @@ const CardItems = ({ card, year, allPhotoShow }) => {
                     m: '10px'
                 }}
             >
+                {/* 画像 */}
+                    <CardMedia
+                        component="img"
+                        image={`/${year}/${card.id}/${ fileName }`}
+                        alt={ card.title.default ? card.title.name[0] : card.title.name[idx] }
+                        sx={{
+                            width: 'auto', maxWidth: '300px',
+                            height: 'auto', maxHeight: '300px'
+                        }}
+                        loading="lazy"
+                    />
+                {/* 画像 */}
 
-                <CardMedia
-                    component="img"
-                    image={`/${year}/${card.id}/${ fileName }`}
-                    alt={ card.title.default ? card.title.name[0] : card.title.name[idx] }
-                    sx={{
-                        width: 'auto', maxWidth: '300px',
-                        height: 'auto', maxHeight: '300px'
-                    }}
-                    loading="lazy"
-                />
+                {/* コンテンツ */}
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography
+                            component="h2"
+                            variant="h5"
+                            gutterBottom
+                        >
+                            { card.title.default ? card.title.name[0] : card.title.name[idx] } { card.title.index && idx }
+                        </Typography>
+                        <Typography>
+                            { card.content.default ? card.content.name[0] : card.content.name[idx] } { card.content.index && idx }
+                        </Typography>
+                    </CardContent>
+                {/* コンテンツ */}
 
-                <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography
-                        component="h2"
-                        variant="h5"
-                        gutterBottom
-                    >
-                        { card.title.default ? card.title.name[0] : card.title.name[idx] } { card.title.index && idx }
-                    </Typography>
-                    <Typography>
-                        { card.content.default ? card.content.name[0] : card.content.name[idx] } { card.content.index && idx }
-                    </Typography>
-                </CardContent>
-
-                <CardActions>
-                    <Button
-                        size="small"
-                        href={`/image/${year}/${card.id}/${ fileName }`}
-                    >
-                        View
-                    </Button>
-                </CardActions>
+                {/* 画像リンクボタン */}
+                    <CardActions>
+                        <Button
+                            size="small"
+                            href={`/image/${year}/${card.id}/${ fileName }`}
+                        >
+                            View
+                        </Button>
+                    </CardActions>
+                {/* 画像リンクボタン */}
 
             </Card>
         )
@@ -79,11 +94,34 @@ const CardItems = ({ card, year, allPhotoShow }) => {
 
 const Gallery = () => {
 
+    // state設定
     const [path, setPath] = useState('')
-    const [allPhotoShow, setAllPhotoShow] = useState(0)
+    const [januaryShow, setJanuaryShow] = useState(false)
+    const [februaryShow, setFebruaryShow] = useState(false)
+    const [marchShow, setMarchShow] = useState(false)
+    const [aprilShow, setAprilShow] = useState(false)
+    const [mayShow, setMayShow] = useState(false)
+    const [juneShow, setJuneShow] = useState(false)
+    const [julyShow, setJulyShow] = useState(false)
+    const [augustShow, setAugustShow] = useState(false)
+    const [septemberShow, setSeptemberShow] = useState(false)
+    const [octoberShow, setOctoberShow] = useState(false)
+    const [novemberShow, setNovemberShow] = useState(false)
+    const [decemberShow, setDecemberShow] = useState(false)
+
+    // パス設定
     const splitPath = path.split('/')
     const year = splitPath[2]
 
+    // 全月のコンテンツ表示状態を配列化
+    const allPhotoShow = [
+        {'1': januaryShow}, {'2': februaryShow}, {'3': marchShow},
+        {'4': aprilShow}, {'5': mayShow}, {'6': juneShow},
+        {'7': julyShow}, {'8': augustShow}, {'9': septemberShow},
+        {'10': octoberShow}, {'11': novemberShow}, {'12': decemberShow},
+    ]
+
+    // 表示する年次を設定
     let gallerys = []
     switch(year) {
         case '2013': gallerys = _2013; break
@@ -97,15 +135,36 @@ const Gallery = () => {
         default: gallerys = _2013; break
     }
 
+    // 月ごとの表示状況を管理
+    const setState = (cond, value) => {
+        switch(cond) {
+            case '1': setJanuaryShow(value); break
+            case '2': setFebruaryShow(value); break
+            case '3': setMarchShow(value); break
+            case '4': setAprilShow(value); break
+            case '5': setMayShow(value); break
+            case '6': setJuneShow(value); break
+            case '7': setJulyShow(value); break
+            case '8': setAugustShow(value); break
+            case '9': setSeptemberShow(value); break
+            case '10': setOctoberShow(value); break
+            case '11': setNovemberShow(value); break
+            case '12': setDecemberShow(value); break
+            default: setJanuaryShow(value); break
+        }
+    }
+
     useEffect(() => {
         setPath(window.location.pathname)
     }, [path])
 
     return gallerys ? (
         <Container sx={{ mt: '40px' }}>
+            {gallerys.map((card, idx) => (
 
-                {gallerys.map((card, idx) => (
-                    <React.Fragment key={`${ card.month }${ idx }`}>
+                <React.Fragment key={`${ card.month }${ idx }`}>
+
+                    {/* 月次タイトル */}
                         <Typography
                             component="h1"
                             variant="h4"
@@ -118,6 +177,9 @@ const Gallery = () => {
                             </SvgIcon>
                             { card.month }
                         </Typography>
+                    {/* 月次タイトル */}
+
+                    {/* Topボタン */}
                         <Button
                             href="#top"
                             variant="contained"
@@ -125,6 +187,21 @@ const Gallery = () => {
                         >
                             Top
                         </Button>
+                    {/* Topボタン */}
+
+                    {/* 展開画像Closeボタン */}
+                        {(allPhotoShow[Number(card.id) - 1][card.id]) && (
+                            <Button
+                                variant="contained"
+                                sx={{ ml: '20px' }}
+                                onClick={() => setState(card.id, false)}
+                            >
+                                Close
+                            </Button>
+                        )}
+                    {/* 展開画像Closeボタン */}
+
+                    {/* 月単位カードコンテナ */}
                         <Card
                             sx={{
                                 display: 'flex',
@@ -134,44 +211,35 @@ const Gallery = () => {
                                 mb: '40px', mt: '15px', p: '10px',
                             }}
                         >
-                            <CardItems
-                                card={card}
-                                year={year}
-                                allPhotoShow={allPhotoShow}
-                            />
-                            {(String(allPhotoShow) !== card.id) && (
-                                <Button
-                                    variant="contained"
-                                    sx={{
-                                        height: '50px',
-                                        m: '20px'
-                                    }}
-                                    onClick={() => {
-                                        switch(card.id) {
-                                            case '1': setAllPhotoShow(1); break
-                                            case '2': setAllPhotoShow(2); break
-                                            case '3': setAllPhotoShow(3); break
-                                            case '4': setAllPhotoShow(4); break
-                                            case '5': setAllPhotoShow(5); break
-                                            case '6': setAllPhotoShow(6); break
-                                            case '7': setAllPhotoShow(7); break
-                                            case '8': setAllPhotoShow(8); break
-                                            case '9': setAllPhotoShow(9); break
-                                            case '10': setAllPhotoShow(10); break
-                                            case '11': setAllPhotoShow(11); break
-                                            case '12': setAllPhotoShow(12); break
-                                            default: setAllPhotoShow(0); break
-                                        }
-                                        window.location.href = '#' + card.month
-                                    }}
-                                >
-                                    View More
-                                </Button>
-                            )}
-                        </Card>
-                    </React.Fragment>
-                ))}
+                            {/* 月単位カードアイテム（複数） */}
+                                <CardItems
+                                    card={card}
+                                    year={year}
+                                    allPhotoShow={allPhotoShow}
+                                />
+                            {/* 月単位カードアイテム（複数） */}
 
+                            {/* 画像展開ボタン */}
+                                {(!allPhotoShow[Number(card.id) - 1][card.id]) && (
+                                    <Button
+                                        variant="contained"
+                                        sx={{
+                                            height: '50px',
+                                            m: '20px'
+                                        }}
+                                        onClick={() => setState(card.id, true)}
+                                    >
+                                        View More
+                                    </Button>
+                                )}
+                            {/* 画像展開ボタン */}
+
+                        </Card>
+                    {/* 月単位カードコンテナ */}
+
+                </React.Fragment>
+
+            ))}
         </Container>
     ) : (
         <Container
